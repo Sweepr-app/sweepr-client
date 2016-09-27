b095d225c2c69ce7b7cdff949395a6d3be32d2d7
Christopher Jennison <cjennison92@gmail.com>
2016-09-27 18:47:37 -0400

    project setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sweeprClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sweeprClientApp
+ */
+angular.module('sweeprClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
